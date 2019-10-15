b1f11bd6af280c3cf2f00cd6c8c0a43d42660eb6
wandatrijhuliana <wandatjh2003@gmail.com>
2019-10-15 15:40:43 +0700

    first commit

---
+++ app/src/main/java/com/wandatjh2003/recycleview_33/MainActivity.java
@@ -0,0 +1,33 @@
+package com.wandatjh2003.recycleview_33;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i =0;i<20;i++){
+            mWordList.add("Word" + mCount++);
+            Log.d("WordList", mWordList.getLast());
+        }
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this,mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
